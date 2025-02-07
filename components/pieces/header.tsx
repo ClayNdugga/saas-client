e2bbe07bbd6ead39fc9b2bf2f67f5bb4ebe75403
clayndugga <clayndugga@gmail.com>
2025-02-07 15:44:19 -0700

    removed unused imports

--- components/pieces/header.tsx
+++ components/pieces/header.tsx
@@ -1,8 +1,9 @@
-import { Book, Menu, Sunset, Trees, Zap, MessageCircleMoreIcon } from "lucide-react";
+import { Menu, MessageCircleMoreIcon } from "lucide-react";
+// import { Book, Menu, Sunset, Trees, Zap, MessageCircleMoreIcon } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+// import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { Button, buttonVariants } from "@/components/ui/button";
 import {
   navigationMenuTriggerStyle,
@@ -15,51 +16,51 @@ import { useRouter } from "next/navigation";
 
 
 
-const subMenuItemsOne = [
-  {
-    title: "Blog",
-    description: "The latest industry news, updates, and info",
-    icon: <Book className="size-5 shrink-0" />,
-  },
-  {
-    title: "Compnay",
-    description: "Our mission is to innovate and empower the world",
-    icon: <Trees className="size-5 shrink-0" />,
-  },
-  {
-    title: "Careers",
-    description: "Browse job listing and discover our workspace",
-    icon: <Sunset className="size-5 shrink-0" />,
-  },
-  {
-    title: "Support",
-    description: "Get in touch with our support team or visit our community forums",
-    icon: <Zap className="size-5 shrink-0" />,
-  },
-];
+// const subMenuItemsOne = [
+//   {
+//     title: "Blog",
+//     description: "The latest industry news, updates, and info",
+//     icon: <Book className="size-5 shrink-0" />,
+//   },
+//   {
+//     title: "Compnay",
+//     description: "Our mission is to innovate and empower the world",
+//     icon: <Trees className="size-5 shrink-0" />,
+//   },
+//   {
+//     title: "Careers",
+//     description: "Browse job listing and discover our workspace",
+//     icon: <Sunset className="size-5 shrink-0" />,
+//   },
+//   {
+//     title: "Support",
+//     description: "Get in touch with our support team or visit our community forums",
+//     icon: <Zap className="size-5 shrink-0" />,
+//   },
+// ];
 
-const subMenuItemsTwo = [
-  {
-    title: "Help Center",
-    description: "Get all the answers you need right here",
-    icon: <Zap className="size-5 shrink-0" />,
-  },
-  {
-    title: "Contact Us",
-    description: "We are here to help you with any questions you have",
-    icon: <Sunset className="size-5 shrink-0" />,
-  },
-  {
-    title: "Status",
-    description: "Check the current status of our services and APIs",
-    icon: <Trees className="size-5 shrink-0" />,
-  },
-  {
-    title: "Terms of Service",
-    description: "Our terms and conditions for using our services",
-    icon: <Book className="size-5 shrink-0" />,
-  },
-];
+// const subMenuItemsTwo = [
+//   {
+//     title: "Help Center",
+//     description: "Get all the answers you need right here",
+//     icon: <Zap className="size-5 shrink-0" />,
+//   },
+//   {
+//     title: "Contact Us",
+//     description: "We are here to help you with any questions you have",
+//     icon: <Sunset className="size-5 shrink-0" />,
+//   },
+//   {
+//     title: "Status",
+//     description: "Check the current status of our services and APIs",
+//     icon: <Trees className="size-5 shrink-0" />,
+//   },
+//   {
+//     title: "Terms of Service",
+//     description: "Our terms and conditions for using our services",
+//     icon: <Book className="size-5 shrink-0" />,
+//   },
+// ];
 
 const Header = () => {
   const router = useRouter();
